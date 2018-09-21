6e2f3da64188b99e413319cf668beffdfe0b5ebe
KVEN <triaglgame@gmail.com>
2018-09-21 22:27:33 +0700

    шаблон листа аккорда

--- NoteAppUI/NoteApp/ChordsList.cs
+++ NoteAppUI/NoteApp/ChordsList.cs
@@ -6,9 +6,30 @@ using System.Threading.Tasks;
 
 namespace NoteApp
 {
-    public class ChordsList
+    public class ChordsList : Chord
     {
+        private List<Chord> _list_of_chords = new List<Chord>();
+        /// <summary>
+        /// добавляет аккорд в список аккоордов
+        /// </summary>
+        /// <param name="chord"> аккорд который добавляют в список</param>
+        public void Add (Chord chord)
+        {
+            _list_of_chords.Add(chord);
+        }
+        /// <summary>
+        /// удаляет аккорд в списке аккордов по названию аккорда
+        /// </summary>
+        /// <param name="_name">название аккорда, который нужно удолить</param>
+        public void Delete(string _name)
+        {
+        }
+       
+        /* public list<Chord> GetChords
+        {   
+            должен вернуть список аккордов, название в лист бокс,
+            начальный лад в бокс текст,
+            массив флагов в условную конструцию которая будет ставить точку в определенной координате если 1 и не будет если 0
 
-
-    }
+        }
 }
