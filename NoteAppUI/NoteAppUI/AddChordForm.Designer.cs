87b38ec835f086fe85b20bf866531abe36ff0ab9
KVEN <trianglgame@gmail.com>
2018-11-17 13:19:57 +0700

    Дизайн2

--- NoteAppUI/NoteAppUI/AddChordForm.Designer.cs
+++ NoteAppUI/NoteAppUI/AddChordForm.Designer.cs
@@ -28,147 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.noteBox = new System.Windows.Forms.PictureBox();
-			this.nameInput = new System.Windows.Forms.TextBox();
-			this.beginInput = new System.Windows.Forms.TextBox();
-			this.nameLabel = new System.Windows.Forms.Label();
-			this.beginLabel = new System.Windows.Forms.Label();
-			this.addChord = new System.Windows.Forms.Button();
-			this.cancel = new System.Windows.Forms.Button();
-			this.name = new System.Windows.Forms.Label();
-			this.begin = new System.Windows.Forms.Label();
-			this.clear = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.noteBox)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// noteBox
-			// 
-			this.noteBox.BackColor = System.Drawing.Color.Moccasin;
-			this.noteBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.noteBox.Location = new System.Drawing.Point(67, 134);
-			this.noteBox.Name = "noteBox";
-			this.noteBox.Size = new System.Drawing.Size(294, 313);
-			this.noteBox.TabIndex = 2;
-			this.noteBox.TabStop = false;
-			this.noteBox.Paint += new System.Windows.Forms.PaintEventHandler(this.NoteBox_Paint);
-			this.noteBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.NoteBox_MouseDown);
-			// 
-			// nameInput
-			// 
-			this.nameInput.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.nameInput.Location = new System.Drawing.Point(141, 46);
-			this.nameInput.Name = "nameInput";
-			this.nameInput.Size = new System.Drawing.Size(100, 24);
-			this.nameInput.TabIndex = 3;
-			this.nameInput.TextChanged += new System.EventHandler(this.NameInput_TextChanged);
-			// 
-			// beginInput
-			// 
-			this.beginInput.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.beginInput.Location = new System.Drawing.Point(141, 89);
-			this.beginInput.Name = "beginInput";
-			this.beginInput.Size = new System.Drawing.Size(100, 24);
-			this.beginInput.TabIndex = 4;
-			this.beginInput.TextChanged += new System.EventHandler(this.BeginInput_TextChanged);
-			// 
-			// nameLabel
-			// 
-			this.nameLabel.AutoSize = true;
-			this.nameLabel.Font = new System.Drawing.Font("Times New Roman", 11F);
-			this.nameLabel.Location = new System.Drawing.Point(12, 53);
-			this.nameLabel.Name = "nameLabel";
-			this.nameLabel.Size = new System.Drawing.Size(123, 17);
-			this.nameLabel.TabIndex = 5;
-			this.nameLabel.Text = "Название аккорда:";
-			// 
-			// beginLabel
-			// 
-			this.beginLabel.AutoSize = true;
-			this.beginLabel.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.beginLabel.Location = new System.Drawing.Point(28, 96);
-			this.beginLabel.Name = "beginLabel";
-			this.beginLabel.Size = new System.Drawing.Size(107, 17);
-			this.beginLabel.TabIndex = 6;
-			this.beginLabel.Text = "Начальный лад:";
-			// 
-			// addChord
-			// 
-			this.addChord.BackColor = System.Drawing.Color.Moccasin;
-			this.addChord.Location = new System.Drawing.Point(309, 64);
-			this.addChord.Name = "addChord";
-			this.addChord.Size = new System.Drawing.Size(80, 33);
-			this.addChord.TabIndex = 7;
-			this.addChord.Text = "Добавить";
-			this.addChord.UseVisualStyleBackColor = false;
-			this.addChord.Click += new System.EventHandler(this.AddChord_Click);
-			// 
-			// cancel
-			// 
-			this.cancel.BackColor = System.Drawing.Color.Moccasin;
-			this.cancel.Location = new System.Drawing.Point(328, 465);
-			this.cancel.Name = "cancel";
-			this.cancel.Size = new System.Drawing.Size(75, 23);
-			this.cancel.TabIndex = 8;
-			this.cancel.Text = "Отменить";
-			this.cancel.UseVisualStyleBackColor = false;
-			this.cancel.Click += new System.EventHandler(this.Сancel_Click);
-			// 
-			// name
-			// 
-			this.name.AutoSize = true;
-			this.name.BackColor = System.Drawing.Color.Moccasin;
-			this.name.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.name.Location = new System.Drawing.Point(190, 163);
-			this.name.Name = "name";
-			this.name.Size = new System.Drawing.Size(0, 21);
-			this.name.TabIndex = 9;
-			// 
-			// begin
-			// 
-			this.begin.AutoSize = true;
-			this.begin.BackColor = System.Drawing.Color.Moccasin;
-			this.begin.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.begin.Location = new System.Drawing.Point(114, 195);
-			this.begin.Name = "begin";
-			this.begin.Size = new System.Drawing.Size(0, 21);
-			this.begin.TabIndex = 10;
-			// 
-			// clear
-			// 
-			this.clear.BackColor = System.Drawing.Color.Moccasin;
-			this.clear.Location = new System.Drawing.Point(60, 453);
-			this.clear.Name = "clear";
-			this.clear.Size = new System.Drawing.Size(75, 23);
-			this.clear.TabIndex = 11;
-			this.clear.Text = "Очистить";
-			this.clear.UseVisualStyleBackColor = false;
-			this.clear.Click += new System.EventHandler(this.Clear_Click);
-			// 
-			// AddChordForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.BurlyWood;
-			this.ClientSize = new System.Drawing.Size(415, 500);
-			this.Controls.Add(this.clear);
-			this.Controls.Add(this.begin);
-			this.Controls.Add(this.name);
-			this.Controls.Add(this.cancel);
-			this.Controls.Add(this.addChord);
-			this.Controls.Add(this.beginLabel);
-			this.Controls.Add(this.nameLabel);
-			this.Controls.Add(this.beginInput);
-			this.Controls.Add(this.nameInput);
-			this.Controls.Add(this.noteBox);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Name = "AddChordForm";
-			this.ShowIcon = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Создание аккорда";
-			this.Load += new System.EventHandler(this.AddChordForm_Load);
-			((System.ComponentModel.ISupportInitialize)(this.noteBox)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.noteBox = new System.Windows.Forms.PictureBox();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.beginInput = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.beginLabel = new System.Windows.Forms.Label();
+            this.addChord = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.Label();
+            this.begin = new System.Windows.Forms.Label();
+            this.clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.noteBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // noteBox
+            // 
+            this.noteBox.BackColor = System.Drawing.Color.Moccasin;
+            this.noteBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.noteBox.Location = new System.Drawing.Point(67, 134);
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(294, 313);
+            this.noteBox.TabIndex = 2;
+            this.noteBox.TabStop = false;
+            this.noteBox.Paint += new System.Windows.Forms.PaintEventHandler(this.NoteBox_Paint);
+            this.noteBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.NoteBox_MouseDown);
+            // 
+            // nameInput
+            // 
+            this.nameInput.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameInput.Location = new System.Drawing.Point(141, 46);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(100, 24);
+            this.nameInput.TabIndex = 3;
+            this.nameInput.TextChanged += new System.EventHandler(this.NameInput_TextChanged);
+            // 
+            // beginInput
+            // 
+            this.beginInput.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.beginInput.Location = new System.Drawing.Point(141, 89);
+            this.beginInput.Name = "beginInput";
+            this.beginInput.Size = new System.Drawing.Size(100, 24);
+            this.beginInput.TabIndex = 4;
+            this.beginInput.TextChanged += new System.EventHandler(this.BeginInput_TextChanged);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.nameLabel.Location = new System.Drawing.Point(12, 53);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(123, 17);
+            this.nameLabel.TabIndex = 5;
+            this.nameLabel.Text = "Название аккорда:";
+            // 
+            // beginLabel
+            // 
+            this.beginLabel.AutoSize = true;
+            this.beginLabel.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.beginLabel.Location = new System.Drawing.Point(28, 96);
+            this.beginLabel.Name = "beginLabel";
+            this.beginLabel.Size = new System.Drawing.Size(107, 17);
+            this.beginLabel.TabIndex = 6;
+            this.beginLabel.Text = "Начальный лад:";
+            // 
+            // addChord
+            // 
+            this.addChord.BackColor = System.Drawing.Color.Moccasin;
+            this.addChord.Location = new System.Drawing.Point(309, 64);
+            this.addChord.Name = "addChord";
+            this.addChord.Size = new System.Drawing.Size(80, 33);
+            this.addChord.TabIndex = 7;
+            this.addChord.Text = "Добавить";
+            this.addChord.UseVisualStyleBackColor = false;
+            this.addChord.Click += new System.EventHandler(this.AddChord_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.BackColor = System.Drawing.Color.Moccasin;
+            this.cancel.Location = new System.Drawing.Point(328, 465);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 8;
+            this.cancel.Text = "Отменить";
+            this.cancel.UseVisualStyleBackColor = false;
+            this.cancel.Click += new System.EventHandler(this.Сancel_Click);
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.BackColor = System.Drawing.Color.Moccasin;
+            this.name.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.name.Location = new System.Drawing.Point(190, 163);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(0, 21);
+            this.name.TabIndex = 9;
+            // 
+            // begin
+            // 
+            this.begin.AutoSize = true;
+            this.begin.BackColor = System.Drawing.Color.Moccasin;
+            this.begin.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.begin.Location = new System.Drawing.Point(105, 195);
+            this.begin.Name = "begin";
+            this.begin.Size = new System.Drawing.Size(0, 21);
+            this.begin.TabIndex = 10;
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.Moccasin;
+            this.clear.Location = new System.Drawing.Point(60, 453);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 11;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // AddChordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(415, 500);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.begin);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.addChord);
+            this.Controls.Add(this.beginLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.beginInput);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.noteBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AddChordForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Создание аккорда";
+            this.Load += new System.EventHandler(this.AddChordForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.noteBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
