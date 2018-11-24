65cd17e0b3cb30ccbd7b1e5eef8ed99ff13338b1
KVEN <triaglgame@gmail.com>
2018-11-24 17:51:03 +0700

    Добавлен класс для тестов Json, ошибка в сравнении индетичных данных

---
+++ NoteAppUI/NoteApp.UnitTests/JsonTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace NoteApp.UnitTests
+{
+	[TestFixture]
+	class JsonTest
+	{
+		[Test(Description = "Тест метода сериализации в файл")]
+		public void TestJsonSaveFile_Method()
+		{
+			var expected = new List<Chord>();
+			expected.Add(new Chord() { Name = "Am", Begin = 1, Frets = new List<int[]>{ new int[] {1,1} } });
+			string path = "C:/test.txt";
+			Json.SaveFile(expected, path);
+
+			var actual = new List<Chord>();
+			
+			using (StreamReader file = File.OpenText(path))
+			{
+				JsonSerializer serializer = new JsonSerializer();
+				actual = (List<Chord>)serializer.Deserialize(file, typeof(List<Chord>));
+			}
+			Assert.AreEqual(expected, actual, "Файл сериализован не правильно");
+		}
+
+		[Test(Description = "Тест метода десириализации из файла")]
+		public void TestJsonLoadFile_Method()
+		{
+
+		}
+	}
+}
