0c5b472b5e4f83961a93ec45f6c1f422c6c63aa5
KVEN <triaglgame@gmail.com>
2018-12-05 23:18:37 +0700

    Создан класс для хранения аккордов

---
+++ NoteAppUI/NoteApp/ListOfChords.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace NoteApp
+{
+	class ListOfChords
+	{
+		/// <summary>
+		/// Контейнер для аккордов
+		/// </summary>
+		private readonly List<Chord> _chords;
+
+		/// <summary>
+		/// Конструктор с пустыми параметрами, инициализирующий список
+		/// </summary>
+		public ListOfChords ()
+		{
+			_chords = new List<Chord>();
+		}
+
+		/// <summary>
+		/// Индексатор
+		/// </summary>
+		/// <param name="index">Индекс аккорда в списке</param>
+		/// <returns>Возвращает аккорд из списка с указаным индексом</returns>
+		public Chord this[int index]
+		{
+			get
+			{
+				return _chords[index];
+			}
+		}
+
+	}
+}
