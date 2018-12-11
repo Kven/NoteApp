c3be7dc10ad9139cf6dad077a2ddc211c167d3ea
KVEN <triaglgame@gmail.com>
2018-12-11 23:36:29 +0700

    тестирование структуры

---
+++ NoteAppUI/ClassLibrary1/CoordinatesStructTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace NoteApp.UnitTests
+{
+	class CoordinatesStructTest
+	{
+		private Coordinates _coordinates;
+
+		[SetUp]
+		public void InitCoordinates()
+		{
+			_coordinates = new Coordinates(55, 55);
+		}
+
+
+		[TestCase(40, "Должно возникать исключение если значение меньше 50", TestName = "Присвоение значение меньше 50 в поле Х")]
+		[TestCase(250, "Должно возникать исключение если значение больше 240", TestName = "Присвоение значение больше 240 в поле Х")]
+		public void TestXCoordinatesStructSet_ArgumentExeption(int wrongValue, string message)
+		{
+			Assert.Throws<ArgumentException>(() => { var coordinates = new Coordinates(wrongValue, 50); }, message);
+		}
+
+		[TestCase(40, "Должно возникать исключение если значение меньше 50", TestName = "Присвоение значение меньше 50 в поле Y")]
+		[TestCase(260, "Должно возникать исключение если значение больше 250", TestName = "Присвоение значение больше 250 в поле Y")]
+		public void TestYCoordinatesStructSet_ArgumentExeption(int wrongValue, string message)
+		{
+			Assert.Throws<ArgumentException>(() => { var coordinates = new Coordinates(50, wrongValue); }, message);
+		}
+
+
+		[Test(Description = "Тестирование на получение значения из координаты X")]
+		public void TestXCoordinatesStructGet_CorrentValue()
+		{
+			int expected = 55;
+
+			Assert.AreEqual(_coordinates.X, expected, "Поле Х не возвращает значение");
+		}
+
+		[Test(Description = "Тестирование на получение значения из координаты У")]
+		public void TestYCoordinatesStructGet_CorrentValue()
+		{
+			int expected = 55;
+
+			Assert.AreEqual(_coordinates.Y, expected, "Поле У не возвращает значение");
+		}
+		
+	}
+}
--- NoteAppUI/NoteApp/CoordinatesStruct.cs
+++ NoteAppUI/NoteApp/CoordinatesStruct.cs
@@ -26,12 +26,12 @@ namespace NoteApp
 			if (x >= 50 && x <= 240)
 				X = x;
 			else
-				throw new Exception("Координата Х не может быть меньше 50 или больше 240");
+				throw new ArgumentException("Координата Х не может быть меньше 50 или больше 240");
 
 			if (y >= 50 && y <= 250)
 				Y = y;
 			else
-				throw new Exception("Координата У не может быть меньше 50 или больше 250");
+				throw new ArgumentException("Координата У не может быть меньше 50 или больше 250");
 		}
 	}
 
